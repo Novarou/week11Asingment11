ca146178738387d885eeb961b691f88322736ca8
jeff <jeff@jeff.jeff>
2020-08-18 18:36:59 -0400

    Initial version.  Calculates averages incorrectly.  Please fix.

---
+++ BadMath/Source.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+float average(int i1, int i2, int i3)
+{
+	return i1 + i2 + i3 / 3.0f;
+}
+
+int main()
+{
+	int n1 =0 , n2 = 0, n3 = 0;
+	cout << "This program calculates the average of three numbers." << endl;
+	cout << "First number: "; cin >> n1;
+	cout << "Second number: "; cin >> n2;
+	cout << "Third number: "; cin >> n2;
+
+	float a = average(n1, n2, n3);
+
+	cout << setprecision(1) << fixed << "The average is " << a << endl;
+
+	return 0;
+}
